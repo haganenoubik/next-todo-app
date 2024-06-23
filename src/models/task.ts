7e06513390eb71e28339005666d86620a36838cf
haganenoubik <haganenoubik@gmail.com>
2024-06-24 01:00:42 +0900

    _idをオプショナルから必須に変更

--- src/models/task.ts
+++ src/models/task.ts
@@ -4,7 +4,7 @@ import mongoose, { Document } from "mongoose";
 // Taskインターフェースを定義
 export interface Task {
   // オプショナルな _id フィールドを追加
-  _id?: string;
+  _id: string;
   // タイトル
   title: string;
   // 説明
