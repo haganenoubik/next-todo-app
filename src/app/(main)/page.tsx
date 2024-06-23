d5fd8ff2d99c57bb2f6b04693730df2e25765032
haganenoubik <haganenoubik@gmail.com>
2024-06-23 18:09:23 +0900

    APIを利用してDBからデータ取得してコンソール出力

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,24 @@
 import TaskCard from "@/components/TaskCard/TaskCard";
+import { TaskDocument } from "@/models/task";
 import Link from "next/link";
 import { MdAddTask } from "react-icons/md";
 
-export default function MainPage() {
+const getAllTasks = async ():Promise<TaskDocument[]> => {
+  const response = await fetch(`${process.env.API_URL}/tasks`, {
+    cache: 'no-store',
+  });
+
+  if (response.status != 200) {
+    throw new Error();
+  }
+
+  const data = await response.json();
+  return data.tasks as TaskDocument[];
+}
+
+export default async function MainPage() {
+  const allTasks = await getAllTasks();
+  console.log(allTasks);
   return (
     <div className="p-8 h-full overflow-y-auto pb-24">
       <header className="flex justify-between items-center">
