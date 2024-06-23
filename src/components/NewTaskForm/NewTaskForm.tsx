3d08f2deeb5a9de930f1ba4de3b123b89ac4256c
haganenoubik <haganenoubik@gmail.com>
2024-06-23 17:19:22 +0900

    タスク作成・編集フォームの説明フィールドの必須入力指定を削除

--- src/components/NewTaskForm/NewTaskForm.tsx
+++ src/components/NewTaskForm/NewTaskForm.tsx
@@ -1,9 +1,28 @@
+"use client";
+
+import { createTask, FormState } from "@/actions/task";
+import { useFormState, useFormStatus } from "react-dom";
+
 const NewTaskForm = () => {
+  const initialState: FormState = { error: "" };
+  const [state, formAction] = useFormState(createTask, initialState);
+
+  const SubmitButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <button
+        type="submit"
+        className="mt-8 p-1 w-full rounded-md text-white bg-sky-700 hover:bg-sky-600 text-sm font-semibold shadow-sm disabled:bg-gray-400"
+        disabled={pending}
+      >
+        作成
+      </button>
+    );
+  };
   return (
     <div className="mt-10 mx-auto w-full max-w-sm">
-      <form action="">
+      <form action={formAction}>
         <div>
-          {/* htmlForの値とinputのidを一致させることでlabelクリック時に対応する入力フォームがフォーカスされる */}
           <label htmlFor="title" className="block text-sm font-medium">
             タイトル
           </label>
@@ -23,7 +42,6 @@ const NewTaskForm = () => {
             type="text"
             id="description"
             name="description"
-            required
             className="block mt-2 py-1.5 w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300"
           />
         </div>
@@ -32,23 +50,19 @@ const NewTaskForm = () => {
             期限
           </label>
           <input
-            //　type属性にdateを指定することで日付選択になる
             type="date"
             id="dueDate"
             name="dueDate"
             min="2020-01-01"
             max="2999-12-31"
-            // フィールドが必須入力とする
             required
             className="block mt-2 py-1.5 w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300"
           />
         </div>
-        <button
-          type="submit"
-          className="mt-8 p-1 w-full rounded-md text-white bg-sky-700 hover:bg-sky-600 text-sm font-semibold shadow-sm"
-          >
-            作成
-        </button>
+        <SubmitButton />
+        {state.error && (
+          <p className="mt-2 text-rose-500 text-sm">{state.error}</p>
+        )}
       </form>
     </div>
   );
